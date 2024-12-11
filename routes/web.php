3fd065190deadc356c69bef32339a0536b414014
mmk4-81 <mmkkarbalaei@gmail.com>
2024-12-11 12:10:43 +0330

    define routs

--- routes/web.php
+++ routes/web.php
@@ -30,15 +30,26 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
 
-
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => 'auth'], function() {
     Route::get('messenger', [MessengerController::class, 'index'])->name('home');
-
-    //profile
     Route::post('profile', [UserProfileController::class, 'update'])->name('profile.update');
-
     // search route
     Route::get('messenger/search', [MessengerController::class, 'search'])->name('messenger.search');
+    // fetch user by id
+    Route::get('messenger/id-info', [MessengerController::class, 'fetchIdInfo'])->name('messenger.id-info');
+    // send message
+    Route::post('messenger/send-message', [MessengerController::class, 'sendMessage'])->name('messenger.send-message');
+    // fetch message
+    Route::get('messenger/fetch-messages', [MessengerController::class, 'fetchMessages'])->name('messenger.fetch-messeges');
+    // fetch contacts
+    Route::get('messenger/fetch-contacts', [MessengerController::class, 'fetchContacts'])->name('messenger.fetch-contacts');
+    Route::get('messenger/update-contact-item', [MessengerController::class, 'updateContactItem'])->name('messenger.update-contact-item');
+    Route::post('messenger/make-seen', [MessengerController::class, 'makeSeen'])->name('messenger.make-seen');
+    // favorite routes
+    Route::post('messenger/favorite', [MessengerController::class, 'favorite'])->name('messenger.favorite');
+    Route::get('messenger/fetch-favorite', [MessengerController::class, 'fetchFavoritesList'])->name('messenger.fetch-favorite');
+    Route::delete('messenger/delete-message', [MessengerController::class, 'deleteMessage'])->name('messenger.delete-message');
+
 });
